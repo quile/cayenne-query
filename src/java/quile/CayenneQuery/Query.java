02ef30bd4b73438b93704764716832f0e48a5796
Kyle Dawkins <kdawkins@fastmail.fm>
2013-11-03 13:04:28 -0800

    Filling in the gaps

--- src/java/quile/CayenneQuery/Query.java
+++ src/java/quile/CayenneQuery/Query.java
@@ -5,6 +5,9 @@ import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.access.DataContext;
 import org.apache.cayenne.query.SelectQuery;
 import org.apache.cayenne.query.Ordering;
+import org.apache.cayenne.query.SortOrder;
+import org.apache.cayenne.exp.Expression;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -15,6 +18,8 @@ class Query {
     private int _offset;
     private boolean _distinct;
     private List<Ordering> _sortOrderings = new ArrayList<Ordering>();
+    private List<Expression> _expressions = new ArrayList<Expression>();
+    private List<Object> _parameters = new ArrayList<Object>();
 
     public ObjectContext _objectContext() {
         if (_objectContext == null) {
@@ -42,6 +47,44 @@ class Query {
 
     public Query offset(int offset) {
         _offset = offset;
+        return this;
+    }
+
+    public Query orderBy(String path, boolean isAscending, boolean ignoreCase) {
+        SortOrder sortOrder = SortOrder.ASCENDING;
+
+        if (isAscending) {
+            if (ignoreCase) {
+                sortOrder = SortOrder.ASCENDING_INSENSITIVE;
+            }
+        } else {
+            if (ignoreCase) {
+                sortOrder = SortOrder.DESCENDING_INSENSITIVE;
+            } else {
+                sortOrder = SortOrder.DESCENDING;
+            }
+        }
+        Ordering o = new Ordering(path, sortOrder);
+        return this;
+    }
+
+    public Query orderBy(String path, boolean isAscending) {
+        return orderBy(path, isAscending, false);
+    }
+
+    public Query orderBy(String path) {
+        return orderBy(path, true, false);
+    }
+
+    public Query filter(String filter, Object... args) {
+        Expression exp = Expression.fromString(filter);
+        _expressions.add(exp);
+
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                _parameters.add(args[i]);
+            }
+        }
         return this;
     }
 
